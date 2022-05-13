0882d1d68d24e7d98b4341667870ca86850fa8ab
Paulo Balzi <paulo.balzi@ifb.edu.br>
2022-05-13 15:58:53 -0300

    validação dos campos datas

--- passagens/validations.py
+++ passagens/validations.py
@@ -10,3 +10,13 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     """Verifica a existẽncia de um dígito na string"""
     if any(char.isdigit() for char in valor_campo):
         lista_de_erros[nome_campo] = 'Não inclua números nesse campo'
+
+def data_ida_maior_data_volta(data_ida, data_volta, lista_de_erros):
+    """verifica se data de ida maior que data de volta"""
+    if data_ida > data_volta:
+        lista_de_erros['data_volta']
+
+def data_ida_menor_data_de_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """verifica se a data de ida é menor que a data da pesquisa"""
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que a data de hoje'
